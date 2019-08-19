50882679465deb90bab66c09a85baf45792ffdcf
ahmad <achehre@yahoo.com>
2019-08-19 11:55:37 +0430

    ToJson method added.

---
+++ src/AtEase.Newtonsoft.Json/NewtonsoftJsonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace AtEase.Newtonsoft.Json
+{
+    public static class NewtonsoftJsonExtensions
+    {
+        public static string ToJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public static TDest To<TDest>(this string value)
+        {
+            return JsonConvert.DeserializeObject<TDest>(value);
+        }
+    }
+}
